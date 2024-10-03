ad79616247a4d6f73726fe08ee45ec6b647f5f7c
jingyuerY <43965944+jingyuerY@users.noreply.github.com>
2024-10-03 15:20:11 +0800

    Add files via upload

---
+++ configs/pdl1_configs.py
@@ -0,0 +1,11 @@
+# data configs
+DF_PATH = "/data/storage/jingyu/simta_plus/pd_l1_0831.csv"
+# DF_PATH = "/data/storage/jingyu/simta_plus/pd_l1_0908.csv"
+
+# tensorboard log directory
+LOG_DIR = "/data/storage/jingyu/simta_plus/logsss"
+
+# model configs
+TEMP_IN_CHANNELS = 129
+STAT_IN_CHANNELS = 21
+NUM_CLASSES = 2
---
+++ configs/physionet_configs.py
@@ -0,0 +1,11 @@
+# data configs
+DF_PATH = "/media/dntech/_mnt_storage/kaiming/workspace/simta_plus/scripts/sepsis_preprocessed_0822.csv"
+OI_STATS_PATH = "/media/dntech/_mnt_storage/kaiming/workspace/simta_plus/scripts/obs_int_stats.csv"
+
+# tensorboard log directory
+LOG_DIR = "/media/dntech/_mnt_storage/kaiming/data/simta_plus/logs"
+
+# model configs
+TEMP_IN_CHANNELS = 34
+STAT_IN_CHANNELS = 6
+NUM_CLASSES = 2
